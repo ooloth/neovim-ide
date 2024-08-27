74f56fe4fd959d1d657f57e89407fdb429a7058d
Michael Uloth <hello@michaeluloth.com>
2024-08-27 12:19:02 -0400

    mini.files: refactor config to be more concise

--- lua/config/plugins/mini-files.lua
+++ lua/config/plugins/mini-files.lua
@@ -1,3 +1,7 @@
+local open_at_current_file = function()
+  require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
+end
+
 -- see: https://github.com/echasnovski/mini.nvim/blob/main/doc/mini-files.txt#L444-L446
 local hide_junk = function(fs_entry)
   return not vim.startswith(fs_entry.name, '.DS_Store')
@@ -5,44 +9,32 @@ end
 
 return {
   'echasnovski/mini.files',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  version = '*',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
-    {
-      '-',
-      function()
-        require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = 'Open file explorer (mini.files)',
-    },
+    { '-', open_at_current_file, desc = 'Open file explorer (mini.files)' },
   },
   opts = {
     -- see: https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-files.md#default-config
     content = {
       filter = hide_junk, -- predicate for which file system entries to show
     },
-
     mappings = {
-      -- to disable a keymap, set it to ''
+      -- to disable a mapping, set it to ''
       close = '<esc>',
       go_in = '',
       go_in_plus = 'l', -- close mini.files when opening a file
       go_out = 'h',
-      go_out_plus = 'H',
+      go_out_plus = '',
       reset = '<BS>',
       show_help = '?',
       synchronize = '<C-y>',
       trim_left = '<',
       trim_right = '>',
     },
-
     options = {
       permanent_delete = true, -- whether to delete permanently or move into module-specific trash
       use_as_default_explorer = true, -- whether to use for editing directories
     },
-
     windows = {
       max_number = 2, -- maximum number of windows to show side by side
       preview = true,
