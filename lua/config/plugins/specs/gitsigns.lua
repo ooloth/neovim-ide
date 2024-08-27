d2cca117b3d4a753e8dcd0321dfcf6acbd27f8bb
Michael Uloth <hello@michaeluloth.com>
2024-08-27 18:50:05 -0400

    gitsigns: enable current line blame

--- lua/config/plugins/specs/gitsigns.lua
+++ lua/config/plugins/specs/gitsigns.lua
@@ -4,6 +4,8 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
+    -- see: https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#installation--usage
+    current_line_blame = true, -- toggle with `:Gitsigns toggle_current_line_blame`
     signs = {
       add = { text = '+' },
       change = { text = '~' },
