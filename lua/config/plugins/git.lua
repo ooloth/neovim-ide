3ae4919d22bc37f11b71bf47cb9ccb5ade36e1c1
Michael Uloth <hello@michaeluloth.com>
2024-08-27 16:22:58 -0400

    gitsigns: move spec to separate file

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -1,14 +1,4 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
--- See `:help gitsigns` to understand what the configuration keys do
 return {
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  -- TODO: get lazygit in here asap
+  require 'config.plugins.specs.gitsigns', -- show git modification symbols in the sign column
 }
---
+++ lua/config/plugins/specs/gitsigns.lua
@@ -0,0 +1,15 @@
+-- Adds git-related signs to the left gutter
+-- See `:help gitsigns`
+
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    signs = {
+      add = { text = '+' },
+      change = { text = '~' },
+      delete = { text = '_' },
+      topdelete = { text = '‾' },
+      changedelete = { text = '~' },
+    },
+  },
+}
