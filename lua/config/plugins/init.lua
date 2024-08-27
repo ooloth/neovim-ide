6835cd86b8b49d432229f82a0dcf1db4908ade38
Michael Uloth <hello@michaeluloth.com>
2024-08-27 16:07:51 -0400

    telescope: update config

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -2,8 +2,6 @@
 return { -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- Use `opts = {}` to force a plugin to be loaded.
-
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
@@ -20,111 +18,6 @@ return { -- NOTE: Plugins can be added with a link (or for a github repo: 'owner
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
-    end,
-  },
-
-  { -- Fuzzy Finder (files, lsp, etc)
-    'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
-    config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
-      require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      }
-
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Help' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Keymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Files' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Select Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'By [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Existing buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = ' [/] in Open Files' })
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = ' [N]eovim files' })
     end,
   },
 
@@ -609,27 +502,4 @@ return { -- NOTE: Plugins can be added with a link (or for a github repo: 'owner
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
 }
---
+++ lua/config/plugins/specs/telescope.lua
@@ -0,0 +1,167 @@
+-- TODO: https://www.lazyvim.org/extras/editor/telescope
+-- TODO: how to restrict searches to certain paths?
+-- TODO: how to include/exclude certain file/folder patterns from a search?
+-- TODO: use nvim-notify extension? does noice replace that?
+
+-- Support opening multiple files in the same picker session:
+-- see: https://github.com/nvim-telescope/telescope.nvim/issues/1048#issuecomment-2142669167
+local select_one_or_multi = function(prompt_bufnr)
+  local actions = require 'telescope.actions'
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+
+  if not vim.tbl_isempty(multi) then
+    actions.close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        if j.lnum ~= nil then
+          vim.cmd(string.format('%s +%s %s', 'edit', j.lnum, j.path))
+        else
+          vim.cmd(string.format('%s %s', 'edit', j.path))
+        end
+      end
+    end
+  else
+    actions.select_default(prompt_bufnr)
+  end
+end
+
+return {
+  'nvim-telescope/telescope.nvim',
+  -- event = 'VimEnter',
+  tag = '0.1.8',
+  dependencies = {
+    -- 'folke/noice.nvim',
+    'nvim-lua/plenary.nvim',
+    'debugloop/telescope-undo.nvim',
+    {
+      -- see: https://github.com/danielfalk/smart-open.nvim?tab=readme-ov-file#installation
+      'danielfalk/smart-open.nvim',
+      branch = '0.2.x',
+      dependencies = {
+        'kkharji/sqlite.lua',
+        'nvim-telescope/telescope-fzy-native.nvim',
+      },
+    },
+  },
+  config = function()
+    -- Telescope is a fuzzy finder that comes with a lot of different things that
+    -- it can fuzzy find! It's more than just a "file finder", it can search
+    -- many different aspects of Neovim, your workspace, LSP, and more!
+    --
+    -- The easiest way to use Telescope, is to start by doing something like:
+    --  :Telescope help_tags
+    --
+    -- After running this command, a window will open up and you're able to
+    -- type in the prompt window. You'll see a list of `help_tags` options and
+    -- a corresponding preview of the help.
+    --
+    -- Two important keymaps to use while in Telescope are:
+    --  - Insert mode: <c-/>
+    --  - Normal mode: ?
+    --
+    -- This opens a window that shows you all of the keymaps for the current
+    -- Telescope picker. This is really useful to discover what Telescope can
+    -- do as well as how to actually do it!
+
+    -- See `:help telescope` and `:help telescope.setup()`
+
+    local actions = require 'telescope.actions'
+    -- local undo_actions = require('telescope-undo.actions')
+
+    require('telescope').setup {
+      defaults = {
+        layout_config = {
+          flex = { width = 0.98, height = 0.97, preview_width = 0.5 },
+          horizontal = { width = 0.98, height = 0.97, preview_width = 0.5 },
+          prompt_position = 'top',
+          vertical = { width = 0.98, height = 0.97, preview_width = 0.5 },
+        },
+        mappings = {
+          i = {
+            -- optionally select/deselect multiple files with <Tab>/<S-Tab>, then open all with <CR>
+            ['<CR>'] = select_one_or_multi, -- still opens just one file if no multi-selection
+            ['<esc>'] = actions.close,
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-h>'] = actions.cycle_history_prev,
+            ['<C-l>'] = actions.cycle_history_next,
+          },
+        },
+        sorting_strategy = 'ascending',
+      },
+      pickers = {
+        help_tags = {
+          mappings = {
+            i = {
+              ['<CR>'] = 'select_vertical',
+            },
+            n = {
+              ['<CR>'] = 'select_vertical',
+            },
+          },
+        },
+      },
+      extensions = {
+        -- ['ui-select'] = {
+        --   require('telescope.themes').get_dropdown(),
+        -- },
+        undo = {
+          -- see: https://github.com/debugloop/telescope-undo.nvim?tab=readme-ov-file#configuration
+          use_delta = false,
+          -- mappings = {
+          --   i = {
+          --     ['<C-cr>'] = undo_actions.restore,
+          --     ['<cr>'] = undo_actions.yank_additions,
+          --     ['<S-cr>'] = undo_actions.yank_deletions,
+          --   },
+          -- },
+        },
+      },
+    }
+
+    -- Load extensions after setup if they're installed
+    local telescope = require 'telescope'
+    -- pcall(telescope.load_extension 'noice')
+    pcall(telescope.load_extension 'smart_open')
+    -- pcall(telescope.load_extension, 'ui-select')
+    pcall(telescope.load_extension 'undo')
+
+    -- See `:help telescope.builtin`
+    local builtin = require 'telescope.builtin'
+    local extensions = telescope.extensions
+
+    vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, { desc = 'Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>,', builtin.command_history, { desc = 'Recent commands' })
+    vim.keymap.set('n', '<leader>gB', builtin.git_branches, { desc = 'Branches' })
+    vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Commits' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = 'Status' })
+    vim.keymap.set('n', '<leader>gS', builtin.git_stash, { desc = 'Stashes' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Recent files' })
+    vim.keymap.set('n', '<leader>s,', builtin.command_history, { desc = 'Recent commands' })
+    vim.keymap.set('n', '<leader>s/', builtin.current_buffer_fuzzy_find, { desc = 'Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>sa', builtin.autocommands, { desc = 'Auto commands' })
+    vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = 'Commands (plugin)' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Diagnostics' })
+    -- stylua: ignore
+    vim.keymap.set('n', '<leader>se', function() builtin.buffers { cwd_only = true, ignore_current_buffer = true } end, { desc = 'Existing buffers' })
+    -- stylua: ignore
+    vim.keymap.set('n', '<leader>sf', function() extensions.smart_open.smart_open { cwd_only = true } end, { desc = 'Files' })
+    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Help' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Keymaps' })
+    -- TODO: '<leader>sl' = 'Links in buffer' (see urlview.lua)
+    vim.keymap.set('n', '<leader>sm', builtin.marks, { desc = 'Marks' })
+    vim.keymap.set('n', '<leader>sM', builtin.man_pages, { desc = 'Man pages' })
+    -- vim.keymap.set('n', '<leader>sn', builtin.noice, { desc = 'Notifications (messages)' })
+    vim.keymap.set('n', '<leader>so', builtin.vim_options, { desc = 'Options' })
+    vim.keymap.set('n', '<leader>sp', builtin.builtin, { desc = 'Telescope pickers' })
+    vim.keymap.set('n', '<leader>sr', builtin.lsp_references, { desc = 'References to symbol' })
+    vim.keymap.set('n', '<leader>ss', builtin.live_grep, { desc = 'String' })
+    -- TODO: '<leader>st' = 'Todo' (see todo-comment.lua)
+    vim.keymap.set('n', '<leader>su', extensions.undo.undo, { desc = 'Undo history' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Word under cursor' })
+    vim.keymap.set('n', '<leader>sz', builtin.resume, { desc = 'Resume last search' })
+    -- stylua: ignore
+    vim.keymap.set('n', '<leader>uC', function() builtin.colorscheme { enable_preview = true } end, { desc = 'Colorschemes (with preview)' })
+  end,
+}
