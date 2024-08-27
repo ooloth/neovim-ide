290a6a723320260e6c01bcc2e75a2767233ace7b
Michael Uloth <hello@michaeluloth.com>
2024-08-27 16:10:15 -0400

    vim-tmux-navigator: install + configure

--- lua/config/plugins/navigating.lua
+++ lua/config/plugins/navigating.lua
@@ -1,4 +1,5 @@
 return {
   require 'config.plugins.specs.mini-files', -- file system editor + explorer
   require 'config.plugins.specs.persistence', -- session manager
+  require 'config.plugins.specs.vim-tmux-navigator', -- move across vim + tmux windows with c-jkhl
 }
