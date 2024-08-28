caed00fd4f870f9970300e11f9bbb5dca1db753d
Michael Uloth <hello@michaeluloth.com>
2024-08-27 20:03:08 -0400

    gitsigns: toggle line blame and deleted lines

--- lua/config/plugins/specs/gitsigns.lua
+++ lua/config/plugins/specs/gitsigns.lua
@@ -3,6 +3,7 @@
 
 return {
   'lewis6991/gitsigns.nvim',
+  event = 'BufRead',
   opts = {
     -- see: https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#installation--usage
     current_line_blame = true, -- toggle with `:Gitsigns toggle_current_line_blame`
@@ -13,5 +14,18 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    on_attach = function(buffer)
+      -- see: https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#keymaps
+      local gs = require 'gitsigns'
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      end
+
+    -- stylua: ignore start
+    map('n', '<leader>gb', function() gs.blame_line { full = true } end, 'Blame (full)')
+    map('n', '<leader>gtb', gs.toggle_current_line_blame, 'Line blame')
+    map('n', '<leader>gtd', gs.toggle_deleted, 'Deleted lines')
+    end,
   },
 }
