4815218d54aa80b42ccfd3d09cd8001b8c187308
Michael Uloth <hello@michaeluloth.com>
2024-08-29 11:03:54 -0400

    astro: fix filename

--- lua/config/plugins/todo/lang/astro.lua
+++ lua/config/plugins/todo/lang/astro.lua
@@ -1,34 +1,40 @@
---  TODO: lsp?
---  TODO: formatting?
---  TODO: linting?
+local extend = require('util').extend
 
 return {
-  -- {
-  -- 'WhoIsSethDaniel/mason-tool-installer.nvim',
-  --   opts = function(_, opts)
-  --     extend(opts.ensure_installed, {})
-  --   end,
-  -- },
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      extend(opts.ensure_installed, { 'astro-language-server', 'prettier' })
+    end,
+  },
 
   {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed or {}, { 'vim', 'vimdoc' })
+      -- see: https://github.com/virchau13/tree-sitter-astro/#troubleshooting
+      extend(opts.ensure_installed, { 'astro', 'css', 'typescript', 'tsx' })
     end,
   },
 
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      -- NOTE: linting comes from eslint-lsp (which already includes vue files by default)
+      servers = {
+        -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#astro
+        astro = {},
+      },
+    },
+  },
 
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+        -- Also need to install prettier-plugin-astro in project
+        -- see: https://github.com/withastro/prettier-plugin-astro#installation
+        astro = { 'prettier' },
+      },
+    },
+  },
 }
