d74b5f4bdc6d0f851f6e21c0619de02d63d36ab7
Michael Uloth <hello@michaeluloth.com>
2024-08-31 10:11:38 -0400

    todo-comments: remove unused code

--- lua/config/plugins/specs/todo-comments.lua
+++ lua/config/plugins/specs/todo-comments.lua
@@ -1,25 +1,20 @@
 -- TODO: https://www.lazyvim.org/plugins/editor#todo-commentsnvim
 -- NOTE: testing
--- FIXME: testing
 -- WARN: testing
 -- FIX: testing
--- HACK: testing
--- PERF: testing
 
 return {
   'folke/todo-comments.nvim',
   event = 'VeryLazy',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
-    -- FIXME: doesn't do anything yet
     -- see: https://github.com/folke/todo-comments.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
     gui_style = {
       fg = 'NONE', -- The gui style to use for the fg highlight group.
       bg = 'NONE', -- The gui style to use for the bg highlight group.
     },
     highlight = {
-      after = 'fg',
-      -- after = '',
+      after = '',
       keyword = 'fg',
       multiline = false,
     },
