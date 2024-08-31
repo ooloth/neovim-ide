d90f4a72be35cb10c59bd2b3296b5adcfc4f1cb8
Michael Uloth <hello@michaeluloth.com>
2024-08-30 20:29:39 -0400

    lazydev/luvit: install + configure as lua-specific tools

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -20,20 +20,6 @@ return {
     end,
   },
 
-  -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
-  { 'Bilal2453/luvit-meta', lazy = true },
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
