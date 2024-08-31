d8b4db5089b77a4bb26d837f757698581528a927
Michael Uloth <hello@michaeluloth.com>
2024-08-31 15:49:40 -0400

    nvim-treesitter-context: wip config

--- lua/config/plugins/todo/ui/nvim-treesitter-context.lua
+++ lua/config/plugins/todo/ui/nvim-treesitter-context.lua
@@ -1,9 +1,19 @@
--- sticky scroll context
+-- Show sticky nested hierarchy at top of screen as you scroll
+
+-- DOCS: https://github.com/nvim-treesitter/nvim-treesitter-context
+-- DOCS: https://www.lazyvim.org/extras/ui/treesitter-context
+
 return {
   'nvim-treesitter/nvim-treesitter-context',
-  after = 'nvim-treesitter',
+  event = 'VeryLazy',
+  -- dependencies = {
+  -- 'nvim-treesitter/nvim-treesitter',
+  -- },
   opts = {
-    -- see: https://github.com/nvim-treesitter/nvim-treesitter-context?tab=readme-ov-file#configuration
+    mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
+    -- on_attach = function()
+    -- vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = '#1e1e2e', underline = false })
+    -- end,
   },
   -- init = function()
   --   vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = '#1e1e2e', underline = false })
