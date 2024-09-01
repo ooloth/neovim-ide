d9bbd0505ee630d9748674562dddfb22c8a6c9f0
Michael Uloth <hello@michaeluloth.com>
2024-08-31 21:53:27 -0400

    bufferline: install + configure for easier editor identification

---
+++ lua/config/plugins/specs/bufferline.lua
@@ -0,0 +1,38 @@
+-- TODO: https://www.lazyvim.org/plugins/ui#bufferlinenvim
+
+return {
+  'akinsho/bufferline.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'catppuccin/nvim',
+    'tiagovla/scope.nvim',
+  },
+  opts = function()
+    -- see `:h bufferline-configuration`
+    local mocha_custom_bg = '#1A1A28'
+
+    return {
+      -- see: https://github.com/catppuccin/nvim?tab=readme-ov-file#integrations
+      highlights = require('catppuccin.groups.integrations.bufferline').get {
+        custom = {
+          mocha = {
+            separator_selected = { fg = mocha_custom_bg },
+            separator_visible = { fg = mocha_custom_bg },
+            separator = { fg = mocha_custom_bg },
+          },
+        },
+      },
+      options = {
+        always_show_bufferline = true,
+        diagnostics = 'nvim_lsp', -- indicate diagnostic issues
+        indicator = { style = 'none' },
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    }
+  end,
+  init = function()
+    -- Only show the current tab's buffers in bufferline
+    require('scope').setup()
+  end,
+}
--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -4,6 +4,7 @@ vim.cmd [[
 ]]
 
 return {
+  require 'config.plugins.specs.bufferline',
   require 'config.plugins.specs.catppuccin',
   require 'config.plugins.specs.fidget',
   require 'config.plugins.specs.todo-comments',
