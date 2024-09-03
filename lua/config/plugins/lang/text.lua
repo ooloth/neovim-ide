368766280d5b5e9298d3bd3037e16cf6001a3c39
Michael Uloth <hello@michaeluloth.com>
2024-09-03 13:20:45 -0400

    text: remove vale linter

--- lua/config/plugins/lang/text.lua
+++ lua/config/plugins/lang/text.lua
@@ -1,19 +1,19 @@
-local extend = require('config.util').extend
+-- local extend = require('config.util').extend
 
 return {
-  {
-    'williamboman/mason.nvim',
-    opts = function(_, opts)
-      extend(opts.ensure_installed, { 'vale' })
-    end,
-  },
+  -- {
+  --   'williamboman/mason.nvim',
+  --   opts = function(_, opts)
+  --     extend(opts.ensure_installed, {  })
+  --   end,
+  -- },
 
-  {
-    'mfussenegger/nvim-lint',
-    opts = {
-      linters_by_ft = {
-        text = { 'vale' },
-      },
-    },
-  },
+  -- {
+  --   'mfussenegger/nvim-lint',
+  --   opts = {
+  --     linters_by_ft = {
+  --       text = {  },
+  --     },
+  --   },
+  -- },
 }
--- lua/config/plugins/specs/nvim-lint.lua
+++ lua/config/plugins/specs/nvim-lint.lua
@@ -11,6 +11,8 @@ return {
     -- see: https://github.com/mfussenegger/nvim-lint/blob/debabca63c0905b59ce596a55a8e33eafdf66342/lua/lint.lua#L51
     -- see: https://github.com/mfussenegger/nvim-lint/issues/411#issuecomment-1763172506
     require('lint').linters_by_ft.markdown = {} -- disable vale
+    require('lint').linters_by_ft.rst = {} -- disable vale
+    require('lint').linters_by_ft.text = {} -- disable vale
 
     -- Create autocommand to trigger the actual linting
     -- see: https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#usage
