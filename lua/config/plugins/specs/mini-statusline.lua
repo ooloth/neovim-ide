87618f8931121af7a228991192b0b0106a92569a
Michael Uloth <hello@michaeluloth.com>
2024-09-03 15:14:01 -0400

    mini.statusline: improve ui

--- lua/config/plugins/specs/mini-statusline.lua
+++ lua/config/plugins/specs/mini-statusline.lua
@@ -1,26 +1,56 @@
--- TODO: filename: remove cwd from left side of filename path
--- TODO: fileinfo: remove unwanted info
--- TODO: lsp: move to right side + show full server name instead of icon + remove "+" separator when 0-1 values
 -- TODO: show active virtual env when in a python file (and a warning if no venv is active)
 
-local empty = function()
-  return ''
-end
+local lsp_attached_servers = function()
+  local attached_servers = vim.lsp.get_clients()
+  local server_names = ''
+  local separator = ''
 
--- Set the section for cursor location to LINE:COLUMN
-local line_colon_column = function()
-  return '%2l:%-2v'
+  for i, v in ipairs(attached_servers) do
+    if i > 1 then
+      separator = ', '
+    end
+    server_names = server_names .. separator .. v.name
+  end
+
+  return server_names
 end
 
 return {
   'echasnovski/mini.statusline',
-  config = function()
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = true }
+  opts = {
+    content = {
+      active = function()
+        -- see: `:h MiniStatusline-example-content`
+        -- see: https://github.com/echasnovski/mini.statusline/blob/main/lua/mini/statusline.lua#L606-L631
+        local statusline = require 'mini.statusline'
 
-    statusline.section_diff = empty
-    statusline.section_git = empty
-    -- statusline.section_fileinfo = empty
-    statusline.section_location = line_colon_column
-  end,
+        local mode, mode_hl = statusline.section_mode { trunc_width = 120 }
+        local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+        local filename = statusline.section_filename { trunc_width = 999 } -- always truncate to the relative path
+        local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 999 } -- always truncate to just the filetype + icon
+        local location = '%2l:%-2v' -- LINE:COLUMN
+        local lsp = lsp_attached_servers()
+        local search = statusline.section_searchcount { trunc_width = 75 }
+
+        local mocha = require 'catppuccin.palettes.mocha'
+        vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', { bg = mocha.surface0 })
+
+        return statusline.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineDevinfo', strings = { diagnostics, lsp } },
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl, strings = { search, location } },
+        }
+      end,
+      inactive = function()
+        -- see: https://github.com/echasnovski/mini.statusline/blob/main/lua/mini/statusline.lua#L633C30-L633C83
+        return '%#MiniStatuslineInactive#%f%=' -- show relative file path instead of absolute path
+      end,
+    },
+    set_vim_settings = false, -- don't override laststatus option
+    use_icons = true,
+  },
 }
