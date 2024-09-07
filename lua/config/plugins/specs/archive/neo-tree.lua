3b6a74078761f772ba1b0c7c9edfc26d59ce7613
Michael Uloth <hello@michaeluloth.com>
2024-09-07 13:48:12 -0400

    neo-tree: archive old config

--- lua/config/plugins/specs/archive/neo-tree.lua
+++ lua/config/plugins/specs/archive/neo-tree.lua
@@ -2,6 +2,12 @@ return {
 
   {
     'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
     keys = function() -- replace all default keys
       return {
         -- see: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/editor.lua
