249b723b657017f24fd3155d60924016e826c6c8
Michael Uloth <hello@michaeluloth.com>
2025-01-24 21:59:59 -0500

    ui: disable bufferline

--- lua/config/plugins/ui.lua
+++ lua/config/plugins/ui.lua
@@ -40,7 +40,7 @@ vim.cmd [[
 ]]
 
 return {
-  require 'config.plugins.specs.bufferline',
+  -- require 'config.plugins.specs.bufferline',
   require 'config.plugins.specs.catppuccin',
   require 'config.plugins.specs.dressing',
   require 'config.plugins.specs.fidget',
