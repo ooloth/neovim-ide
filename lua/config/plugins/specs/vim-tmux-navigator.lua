ad4f40330935ce7bc05c5aaa8d16a79caf4645ea
Michael Uloth <hello@michaeluloth.com>
2025-02-03 22:23:50 -0500

    fix: compensate for c-j/k outputting c-n/p on Air

--- lua/config/plugins/specs/vim-tmux-navigator.lua
+++ lua/config/plugins/specs/vim-tmux-navigator.lua
@@ -12,8 +12,10 @@ return {
   },
   keys = {
     { '<c-h>', '<cmd><c-u>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><c-u>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><c-u>TmuxNavigateUp<cr>' },
+    { '<c-j>', '<cmd>TmuxNavigateDown<cr>' },
+    { '<c-n>', '<cmd>TmuxNavigateDown<cr>' }, -- compensate for c-j outputting c-n on Air
+    { '<c-k>', '<cmd>TmuxNavigateUp<cr>' },
+    { '<c-p>', '<cmd>TmuxNavigateUp<cr>' }, -- compensate for c-k outputting c-p on Air
     { '<c-l>', '<cmd><c-u>TmuxNavigateRight<cr>' },
     { '<c-\\>', '<cmd><c-u>TmuxNavigatePrevious<cr>' },
   },
