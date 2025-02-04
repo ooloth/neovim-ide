80d86e0847b869b31ead5ee25ca44f129c0d0139
Michael Uloth <hello@michaeluloth.com>
2025-02-03 22:31:55 -0500

    snacks-lazygit: leader-gg to open; leader-gh to view current file commit history

--- lua/config/plugins/specs/snacks-lazygit.lua
+++ lua/config/plugins/specs/snacks-lazygit.lua
@@ -4,11 +4,21 @@ return {
     lazygit = {
       -- see: https://github.com/folke/snacks.nvim/blob/main/docs/lazygit.md
       config = {
-        os = { editPreset = 'nvim-remote' },
         gui = {
           nerdFontsVersion = '', -- set to an empty string "" to disable icons
         },
       },
+      win = {
+        -- see: https://github.com/folke/snacks.nvim/blob/main/docs/win.md
+        height = 0,
+        width = 0,
+      },
     },
+  },
+  keys = {
+    -- stylua: ignore start
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gh", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    -- stylua: ignore end
   },
 }
