6a8f0a580e4d3706bcc588bb90dd72a6e5ee1ce3
Michael Uloth <hello@michaeluloth.com>
2025-02-05 20:27:57 -0500

    lazygit: disable log file keymap in favor of snacks.picker version

--- lua/config/plugins/specs/snacks-lazygit.lua
+++ lua/config/plugins/specs/snacks-lazygit.lua
@@ -8,17 +8,13 @@ return {
           nerdFontsVersion = '', -- set to an empty string "" to disable icons
         },
       },
-      win = {
-        -- see: https://github.com/folke/snacks.nvim/blob/main/docs/win.md
-        height = 0,
-        width = 0,
-      },
+      -- see: https://github.com/folke/snacks.nvim/blob/main/docs/win.md
+      win = { height = 0, width = 0 },
     },
   },
   keys = {
     -- stylua: ignore start
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-    { "<leader>gh", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
     -- stylua: ignore end
   },
 }
