65a91a50e5acb0e4caa20b73156e89074dce3bd2
Michael Uloth <hello@michaeluloth.com>
2025-02-05 19:34:34 -0500

    terminal: remove c-hjkl mappings that cause weird bugs
    For example, if I absent mindedly c-j in lazygit, I simply can't get my
    cursor back or close the terminal pop up. I think that's because I've
    jumped out of the snacks-terminal float to a different window. No good.

--- lua/config/plugins/terminal.lua
+++ lua/config/plugins/terminal.lua
@@ -1,12 +1,6 @@
 local set = vim.keymap.set
 
 set('t', '<esc>', '<c-\\><c-n>', { desc = 'Enter normal mode' })
-set('t', '<c-h>', '<cmd>wincmd h<cr>', { desc = 'Go to left window' })
-set('t', '<c-j>', '<cmd>wincmd j<cr>', { desc = 'Go to lower window' })
-set('t', '<c-n>', '<cmd>wincmd j<cr>', { desc = 'Go to lower window' }) -- compensate for c-j outputting c-n on Air
-set('t', '<c-k>', '<cmd>wincmd k<cr>', { desc = 'Go to upper window' })
-set('t', '<c-p>', '<cmd>wincmd k<cr>', { desc = 'Go to upper window' }) -- compensate for c-k outputting c-p on Air
-set('t', '<c-l>', '<cmd>wincmd l<cr>', { desc = 'Go to right window' })
 set('t', '<c-/>', '<cmd>close<cr>', { desc = 'Hide Terminal' })
 set('t', '<c-_>', '<cmd>close<cr>', { desc = 'which_key_ignore' })
 
