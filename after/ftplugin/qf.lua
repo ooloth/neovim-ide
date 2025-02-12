4df3b85acc058530d9e9b2f91dcfa9fb56692297
Michael Uloth <hello@michaeluloth.com>
2025-02-11 21:02:14 -0500

    help/qf: close with q or esc + exclude from jumplist

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,4 +1,3 @@
-vim.opt_local.conceallevel = 0 -- show concealable characters
 vim.opt_local.buflisted = false -- exclude from jump list
 
 vim.keymap.set('n', '<esc>', '<cmd>close<cr>', { buffer = true, silent = true })
