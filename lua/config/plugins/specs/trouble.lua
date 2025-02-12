ebe02f79b916bb3d694f5242adb0fd1fd10b2069
Michael Uloth <hello@michaeluloth.com>
2025-02-11 21:04:20 -0500

    trouble: leader-x to toggle

--- lua/config/plugins/specs/trouble.lua
+++ lua/config/plugins/specs/trouble.lua
@@ -21,20 +21,6 @@ return {
     warn_no_results = false, -- show a warning when there are no results
   },
   keys = {
-    { '<leader>cl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', desc = 'LSP Definitions / references / ... (Trouble)' },
-    { '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>', desc = 'Symbols (Trouble)' },
-    { '<leader>xL', '<cmd>Trouble loclist toggle<cr>', desc = 'Location List (Trouble)' },
-    { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', desc = 'Quickfix List (Trouble)' },
-    { '<leader>xa', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (all)' },
-    { '<leader>xx', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', desc = 'Diagnostics (editor)' },
-    -- { '<leader>xx', '<cmd>TroubleToggle document_diagnostics<cr>', desc = 'Diagnostics (file)' },
-    -- { '<leader>xa', '<cmd>TroubleToggle workspace_diagnostics<cr>', desc = 'Diagnostics (all)' },
-    -- { '<leader>xX', false },
-    -- { '<leader>xL', false },
-    -- { '<leader>xQ', false },
+    { '<leader>x', '<cmd>Trouble diagnostics toggle<cr>', desc = 'Diagnostics (all)' },
   },
-  init = function()
-    vim.keymap.set('n', '<leader>xl', '<cmd>lopen<cr>', { desc = 'Location List' })
-    vim.keymap.set('n', '<leader>xq', '<cmd>copen<cr>', { desc = 'Quickfix List' })
-  end,
 }
