89235732236c9bde63b56029def6e67707512bda
Michael Uloth <hello@michaeluloth.com>
2025-02-11 22:34:20 -0500

    snacks-zen: customize zoom indicator

--- lua/config/plugins/specs/snacks-zen.lua
+++ lua/config/plugins/specs/snacks-zen.lua
@@ -3,7 +3,15 @@
 return {
   'folke/snacks.nvim',
   opts = {
-    zen = { enabled = true },
+    zen = {
+      enabled = true,
+    },
+    styles = {
+      zoom_indicator = {
+        text = ' 󰊓 ZOOMED 󰊓   ',
+        minimal = false,
+      },
+    },
   },
   keys = {
     { '<leader>wm', function() Snacks.zen.zoom() end, desc = 'Maximize (toggle)' },
