ccf573b9f799a23e775aee94697be40764d61053
Michael Uloth <hello@michaeluloth.com>
2025-02-13 21:48:37 -0500

    snacks(zen): leader-uz to toggle zen mode

--- lua/config/plugins/specs/snacks-zen.lua
+++ lua/config/plugins/specs/snacks-zen.lua
@@ -3,7 +3,7 @@
 return {
   'folke/snacks.nvim',
   opts = {
-    zen = {},
+    zen = {}, -- see: https://github.com/folke/snacks.nvim/blob/main/docs/zen.md#%EF%B8%8F-config
     styles = {
       zoom_indicator = {
         text = ' 󰊓 ZOOMED 󰊓   ',
@@ -13,6 +13,7 @@ return {
   },
   keys = {
     { '<leader>em', function() Snacks.zen.zoom() end, desc = 'Maximize (toggle)' },
+    { '<leader>uz', function() Snacks.zen.zen() end, desc = 'Zen mode (toggle)' },
     { '<leader>wm', function() Snacks.zen.zoom() end, desc = 'Maximize (toggle)' },
   },
 }
