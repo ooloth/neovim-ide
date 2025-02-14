4d92ba2b2520839c02fdcc7590c49b6eeb63105d
Michael Uloth <hello@michaeluloth.com>
2025-02-13 20:41:56 -0500

    help: always move help window to right side

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,5 +1,14 @@
+vim.cmd('wincmd L') -- move help window to the right
+
 vim.opt_local.conceallevel = 0 -- show concealable characters
 vim.opt_local.buflisted = false -- exclude from jump list
 
 vim.keymap.set('n', '<esc>', '<cmd>close<cr>', { buffer = true, silent = true })
 vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = true, silent = true })
+
+-- Move subsequent help windows to the right
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Help window on right side',
+  buffer = 0,
+  callback = function() vim.cmd('wincmd L') end,
+})
