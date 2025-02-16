687f2229a5fed7f1dc37298536763a3e3bc23806
Michael Uloth <hello@michaeluloth.com>
2025-02-15 21:56:48 -0500

    trouble: always show results title and count

--- lua/config/plugins/specs/trouble.lua
+++ lua/config/plugins/specs/trouble.lua
@@ -13,19 +13,18 @@ return {
     },
     modes = {
       diagnostics = {
+        -- see: https://github.com/folke/trouble.nvim/blob/main/lua/trouble/sources/diagnostics.lua
         format = '{severity_icon}{message:md} {item.source} {code} {filename} {pos}',
-        -- groups = {
-        --   { 'cmd', format = '{hl:Title}Diagnostics in open editors{hl} {count}' },
-        -- },
-        groups = {}, -- no tree view (every line is actionable)
+        groups = {
+          { 'cmd', format = '{hl:Title}Diagnostics (open editors){hl} {count}' },
+        },
       },
       todo = {
         -- see: https://github.com/folke/todo-comments.nvim/blob/main/lua/trouble/sources/todo.lua
         format = '{todo_icon} {text} {filename} {pos}',
-        -- groups = {
-        --   { 'cmd', format = '{hl:Title}TODO comments in project{hl} {count}' },
-        -- },
-        groups = {}, -- no tree view (every line is actionable)
+        groups = {
+          { 'cmd', format = '{hl:Title}TODO comments (project){hl} {count}' },
+        },
         sort = { 'tag', 'filename', 'pos', 'message' },
       },
     },
