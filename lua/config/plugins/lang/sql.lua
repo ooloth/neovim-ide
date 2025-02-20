f395fa119c87bb845cc6a41d53b8df7658781639
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:50:37 -0500

    sql: remove unused code

--- lua/config/plugins/lang/sql.lua
+++ lua/config/plugins/lang/sql.lua
@@ -4,33 +4,10 @@
 -- TODO: linting?
 
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'sql' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {
-  --     },
-  --   },
-  -- },
 }
