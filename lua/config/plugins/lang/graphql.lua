73f65d7eef5bc91e27f29cc7da2515b5d0aa8b0e
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:48:36 -0500

    graphql: remove unused code

--- lua/config/plugins/lang/graphql.lua
+++ lua/config/plugins/lang/graphql.lua
@@ -1,29 +1,10 @@
---  TODO: lsp
---  TODO: linting
---  TODO: dap?
-
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'graphql' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
 
   {
     'stevearc/conform.nvim',
