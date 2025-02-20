4bd89c3ee3fbd353a34ca53cab93fb442cdfc602
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:51:12 -0500

    toml: add taplo lsp

--- lua/config/plugins/lang/toml.lua
+++ lua/config/plugins/lang/toml.lua
@@ -1,16 +1,7 @@
--- TODO: https://www.lazyvim.org/extras/lang/toml
--- TODO: lsp?
 -- TODO: formatting?
 -- TODO: linting?
 
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
@@ -18,18 +9,12 @@ return {
     },
   },
 
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        taplo = {},
+      },
+    },
+  },
 }
