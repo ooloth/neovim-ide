7705f5f4159092ec32ec63bf06cfa9e8d3c0d964
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:50:00 -0500

    rego: remove unused code

--- lua/config/plugins/lang/rego.lua
+++ lua/config/plugins/lang/rego.lua
@@ -3,32 +3,10 @@
 --  TODO: linting?
 
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'rego' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
 }
