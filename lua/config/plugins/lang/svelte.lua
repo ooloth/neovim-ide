c43d1c6fda9abb36cb716fa5b6c323424efd67cd
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:50:44 -0500

    svelte: remove unused code

--- lua/config/plugins/lang/svelte.lua
+++ lua/config/plugins/lang/svelte.lua
@@ -6,32 +6,10 @@
 -- TODO: dap?
 
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'svelte' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
 }
