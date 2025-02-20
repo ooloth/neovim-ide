135407f4831aca8913b9a8bfd8eadb98df216d1f
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:49:53 -0500

    pug: remove unused code

--- lua/config/plugins/lang/pug.lua
+++ lua/config/plugins/lang/pug.lua
@@ -1,34 +1,8 @@
---  TODO: lsp?
---  TODO: formatting?
---  TODO: linting?
-
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'pug' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
 }
