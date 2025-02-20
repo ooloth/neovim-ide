308e58f906e1b6c527ace2d258d14e5cc6584130
Michael Uloth <hello@michaeluloth.com>
2025-02-19 23:47:58 -0500

    git: remove unused code

--- lua/config/plugins/lang/git.lua
+++ lua/config/plugins/lang/git.lua
@@ -1,34 +1,13 @@
 -- TODO: https://www.lazyvim.org/extras/lang/git
---  TODO: lsp?
---  TODO: linting?
+-- TODO: lsp?
+-- TODO: linting?
+-- TODO: formatting?
 
 return {
-  -- {
-  --   'williamboman/mason-tool-installer.nvim',
-  --   opts = {
-  --     ensure_installed = {},
-  --   },
-  -- },
-
   {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       ensure_installed = { 'diff', 'git_config', 'git_rebase', 'gitattributes', 'gitcommit', 'gitignore' },
     },
   },
-
-  -- {
-  --   'neovim/nvim-lspconfig',
-  --   opts = {
-  --     servers = {
-  --     },
-  --   },
-  -- },
-
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     formatters_by_ft = {},
-  --   },
-  -- },
 }
